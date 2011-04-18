9cfc4c07d413060677de2b030f9dac1ab5f1f231
David Chawei Hsu <chawei.hsu@gmail.com>
2011-04-18 08:35:08 -0700

    add path

--- exhibition_server.rb
+++ exhibition_server.rb
@@ -17,17 +17,47 @@ class ExhibitionServer
     @client = OSC::Client.new( 'localhost', PORT )
     puts "Start listening at #{PORT}"
     
+    @server.add_method '/statement/*' do | message |
+      puts message.inspect
+      begin
+        if match = message.address.match(/\/statement\/(.*)/)
+          @browser.link(:id, "statement_#{match[1]}").click
+        end
+      rescue
+        "can't hit the click"
+      end
+    end
+    
     @server.add_method '/search/*' do | message |
       puts message.inspect
-      if match = message.address.match(/\/search\/(.*)/)
-        @browser.link(:id, "search_#{match[1]}").click
+      begin
+        if match = message.address.match(/\/search\/(.*)/)
+          @browser.link(:id, "search_#{match[1]}").click
+        end
+      rescue
+        "can't hit the click"
       end
     end
     
     @server.add_method '/magnet/*' do | message |
       puts message.inspect
-      if match = message.address.match(/\/magnet\/(.*)/)
-        @browser.link(:id, "magnet_#{match[1]}").click
+      begin
+        if match = message.address.match(/\/magnet\/(.*)/)
+          @browser.link(:id, "magnet_#{match[1]}").click
+        end
+      rescue
+        "can't hit the click"
+      end
+    end
+    
+    @server.add_method '/monster/*' do | message |
+      puts message.inspect
+      begin
+        if match = message.address.match(/\/monster\/(.*)/)
+          @browser.link(:id, "monster_#{match[1]}").click
+        end
+      rescue
+        "can't hit the click"
       end
     end
 
